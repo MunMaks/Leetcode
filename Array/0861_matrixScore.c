29c14bee6d731ab55229f46e1ccd0628ebb750b9
Maks Munait <98954111+MunMaks@users.noreply.github.com>
2024-05-18 23:01:14 +0200

    Create 0861_matrixScore.c

---
+++ Array/0861_matrixScore.c
@@ -0,0 +1,37 @@
+/*
+861. Score After Flipping Matrix
+https://leetcode.com/problems/score-after-flipping-matrix/description/
+
+You are given an m x n binary matrix grid.
+
+A move consists of choosing any row or column and toggling each value in that row or column (i.e., changing all 0's to 1's, and all 1's to 0's).
+
+Every row of the matrix is interpreted as a binary number, and the score of the matrix is the sum of these numbers.
+
+Return the highest possible score after making any number of moves (including zero moves).
+
+Example 1:
+Input: grid = [[0,0,1,1],[1,0,1,0],[1,1,0,0]]
+Output: 39
+Explanation: 0b1111 + 0b1001 + 0b1111 = 15 + 9 + 15 = 39
+
+Example 2:
+Input: grid = [[0]]
+Output: 1
+*/
+
+int matrixScore(int** grid, int gridSize, int* gridColSize) {
+    int n = gridSize, m = *gridColSize, res = 0, j = 0, val = 0, set = 0, i = 0;
+    res = (1 << (m - 1)) * n;
+    for (j = 1; j < m; ++j){
+        val = 1 << (m - 1 - j);
+        set = 0;
+        for (i = 0; i < n; ++i){
+            if (grid[i][j] == grid[i][0]) {
+                set++;
+            }
+        }
+        res += (set > n - set) ? (set * val) : ((n-set) * val);
+    }
+    return res;
+}
