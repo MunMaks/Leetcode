51ff8c7f7c0d52a6856aea1f5a46287c4d047951
Maks Munait <98954111+MunMaks@users.noreply.github.com>
2024-05-18 22:57:59 +0200

    Create 2331_evaluateTree.c

---
+++ Tree/2331_evaluateTree.c
@@ -0,0 +1,47 @@
+/*
+2331. Evaluate Boolean Binary Tree
+https://leetcode.com/problems/evaluate-boolean-binary-tree/description/
+
+You are given the root of a full binary tree with the following properties:
+
+Leaf nodes have either the value 0 or 1, where 0 represents False and 1 represents True.
+Non-leaf nodes have either the value 2 or 3, where 2 represents the boolean OR and 3 represents the boolean AND.
+The evaluation of a node is as follows:
+
+If the node is a leaf node, the evaluation is the value of the node, i.e. True or False.
+Otherwise, evaluate the node's two children and apply the boolean operation of its value with the children's evaluations.
+Return the boolean result of evaluating the root node.
+
+A full binary tree is a binary tree where each node has either 0 or 2 children.
+
+A leaf node is a node that has zero children.
+
+Example 1:
+Input: root = [2,1,3,null,null,0,1]
+Output: true
+Explanation: The above diagram illustrates the evaluation process.
+The AND node evaluates to False AND True = False.
+The OR node evaluates to True OR False = True.
+The root node evaluates to True, so we return true.
+
+Example 2:
+Input: root = [0]
+Output: false
+Explanation: The root node is a leaf node and it evaluates to false, so we return false.
+*/
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     struct TreeNode *left;
+ *     struct TreeNode *right;
+ * };
+ */
+bool evaluateTree(struct TreeNode* root) {
+    if ( !root->val || 1 == root->val) { return root->val; }
+
+    return (root->val == 2) ?
+            (evaluateTree(root->left) == 1) || (evaluateTree(root->right) == 1) : 
+            (evaluateTree(root->left) == 1) && (evaluateTree(root->right) == 1) ;
+}
